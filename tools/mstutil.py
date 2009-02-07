69fec3201e672a99c96c79ae43ab8bf7c0104b77
David Underhill <dgu@cs.stanford.edu>
2009-02-06 21:17:48 -0800

    bug fix: mstutil.py did not find the script path correctly

--- tools/mstutil.py
+++ tools/mstutil.py
@@ -1,12 +1,20 @@
 import os, random
 
+def get_path_to_tools_root():
+    """Gets the path to the root of the MST tools directory."""
+    # strip off 'mstutil.py'
+    if __file__[0:1] == '/':
+        return __file__[:-10]
+    else:
+        return os.getcwd() + '/' + __file__[:-10]
+
 def get_path_to_project_root():
     """Gets the path to the root of the MST project."""
-    return os.getcwd() + '/../'
+    return get_path_to_tools_root()[:-6] # strip off 'tools/'
 
 def get_path_to_mst_binary():
     """Gets the path to the mst solver binary."""
-    return os.getcwd() + '/../src/mst'
+    return get_path_to_project_root() + 'src/mst'
 
 def is_mst_binary_accessible():
     """Returns whether the mst solver binary can be accessed."""
@@ -19,3 +27,6 @@ def random_tmp_filename(sz):
 def random_filename(sz):
     """Returns a random string of the specified length composed of letters and/or numbers."""
     return "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789") for _ in range(sz)])
+
+if __name__ == "__main__":
+    print get_path_to_project_root()
