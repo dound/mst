2c61f4be8b16b1252a4e8b017a5da98bc08b9264
David Underhill <dgu@cs.stanford.edu>
2009-02-06 17:00:29 -0800

    generate_input.py now allows precision of output to be specified (default=1)

--- tools/generate_input.py
+++ tools/generate_input.py
@@ -3,19 +3,22 @@
 from optparse import OptionParser
 import sys
 
-def gen_random_edge_lengths(num_verts, num_edges, min_edge_len, max_edge_len):
-    print "gen_random_edge_lengths: m=%d n=%d min=%.1f max=%.1f" % (num_verts, num_edges, 
-                                                                    min_edge_len, max_edge_len)
+def gen_random_edge_lengths(num_verts, num_edges, min_edge_len, max_edge_len, precision):
+    print "gen_random_edge_lengths: m=%d n=%d min=%.1f max=%.1f prec=%d" % (num_verts, num_edges, 
+                                                                            min_edge_len, max_edge_len, precision)
 
-def gen_random_vertex_positions(num_verts, num_edges, num_dims, min_pos, max_pos):
-    print "gen_random_vertex_positions: m=%d n=%d d=%d min=%.1f max=%.1f" % (num_verts, num_edges, 
-                                                                             num_dims, min_pos, max_pos)
+def gen_random_vertex_positions(num_verts, num_edges, num_dims, min_pos, max_pos, precision):
+    print "gen_random_vertex_positions: m=%d n=%d d=%d min=%.1f max=%.1f prec=%d" % (num_verts, num_edges, 
+                                                                                     num_dims, min_pos, max_pos, precision)
 
 def main():
     usage = "usage: %prog [options] NUM_VERTICES\nGenerates a connected graph with no self-loops or parallel edges."
     parser = OptionParser(usage)
     parser.add_option("-n", "--num-edges",
                       help="number of edges to put in the graph [default: complete graph]")
+    parser.add_option("-p", "--precision",
+                      type="int", default=1,
+                      help="number of decimal points to specify for edge weights [default: %default]")
     parser.add_option("-e", "--edge-weight-range",
                       metavar="MIN,MAX", 
                       help="range of edge weights [default: >=0.1]")
@@ -47,6 +50,11 @@ def main():
     except ValueError:
         parser.error("-n must either be an integer or 'complete'")
 
+    if options.precision <= 0:
+        parser.error("-p must be at least 1")
+    elif options.precision > 15:
+        parser.error("-p must be no more than 15 (doubles cannot accurately represent more than this)")
+
     if options.style is not None:
         parser.error("option -s is not yet supported")
 
@@ -64,7 +72,7 @@ def main():
         if num_dims < 0:
             parser.error("option -v requires dimensionality to be a strictly positive integer")
 
-        gen_random_vertex_positions(num_verts, num_edges, num_dims, min_pos, max_pos)
+        gen_random_vertex_positions(num_verts, num_edges, num_dims, min_pos, max_pos, options.precision)
         return
 
     # default: randomly choose edge weights in some range
@@ -75,7 +83,7 @@ def main():
         except ValueError:
             parser.error("option -e requires its arguments to be in the form float,float")
 
-        if min_pos < 0.1:
+        if min_pos <= 0.1:
             parser.error("option -e requires minimum edge length to be >= 0.1")
         if min_pos > max_pos:
             parser.error("option -e requires the minimum edge length < maximum edge length")
@@ -84,7 +92,7 @@ def main():
         min_pos = 0.1
         max_pos = float(sys.maxint)
 
-    gen_random_edge_lengths(num_verts, num_edges, min_pos, max_pos)
+    gen_random_edge_lengths(num_verts, num_edges, min_pos, max_pos, options.precision)
 
 if __name__ == "__main__":
     sys.exit(main())
