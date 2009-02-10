2234547977ee1ed6854fd15386d2ac4ef9e291cd
David Underhill <dgu@cs.stanford.edu>
2009-02-10 01:54:34 -0800

    check_output.py now logs the result when checking outputs

--- tools/check_output.py
+++ tools/check_output.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from data import DataSet, InputSolution
+from data import DataError, DataSet, InputSolution, CorrResult, CORRECT, INCORRECT
 from generate_input import ExtractInputFooterError, extract_input_footer
 from mstutil import get_path_to_checker_binary, random_tmp_filename
 from optparse import OptionParser
@@ -46,9 +46,9 @@ def __compute_mst_weight(input_graph, corr_file):
         raise CheckerError("checker error: failed to generate output for " + input_graph)
 
 def get_and_log_mst_weight_from_checker(input_graph, force_recompute=False):
-    """Returns the weight of input_graph's MST according to the checker.  If
-    force_recompute is not True, then it will check the input log cache to see
-    if we already know the answer first.  Logs the result."""
+    """Returns the a 2-tuple of (input, weight).  If force_recompute is not
+    True, then it will check the input log cache to see if we already know the
+    answer first.  Logs the result."""
     try:
         ti = extract_input_footer(input_graph)
     except ExtractInputFooterError, e:
@@ -64,7 +64,7 @@ def get_and_log_mst_weight_from_checker(input_graph, force_recompute=False):
         # see if we already know the answer
         if not force_recompute:
             if input_soln.has_mst_weight():
-                return input_soln.mst_weight  # cache hit!
+                return (ti, input_soln.mst_weight)  # cache hit!
     else:
         # if we weren't tracking the input before, don't start now
         do_log = False
@@ -74,35 +74,43 @@ def get_and_log_mst_weight_from_checker(input_graph, force_recompute=False):
     if do_log:
         if input_soln.update_mst_weight(w):
             ds.save_to_file(logfn)
-    return w
+    return (ti, w)
 
-def check(input_graph, output_to_test, tolerance, force_recompute, do_log):
+def check(input_graph, output_to_test, tolerance, force_recompute, rev=None, run=None):
     """Checks whether the MST weight of input_graph matches output_to_test to
     the specified tolerance.
     @param force_recompute  whether the checker's MST weight can come from cache
-    @param do_log           whether to log the result of the correctness check
+    @param rev              what revision to log this result under
+    @param run              what run number to log this result under
     """
     ans_out = extract_answer(output_to_test)
-    ans_corr = get_and_log_mst_weight_from_checker(input_graph, force_recompute, do_log)
+    (ti, ans_corr) = get_and_log_mst_weight_from_checker(input_graph, force_recompute)
 
     # are they the same?
     fmt = '%.' + tolerance + 'f'
     str_ans_corr = fmt % ans_corr
     str_ans_out = fmt % ans_out
     if str_ans_corr == str_ans_out:
-        code = 0    # succcess!
+        outcome = CORRECT
     else:
         print >> sys.stderr, "correctness FAILED: %s (correct is %s, output had %s)" % (input_graph, str_ans_corr, str_ans_out)
-        code = -1
+        outcome = INCORRECT
 
     # log the result of the correctness check
-    if do_log:
-        pass # TODO
+    if rev is not None and run is not None:
+        data = CorrResult(ti.dims, ti.min, ti.max, ti.num_verts, ti.num_edges, ti.seed, rev, run, outcome)
+        try:
+            DataSet.add_data_to_log_file(data)
+        except DataError, e:
+            fmt = "Unable to log result to file %s (correct is %s, output had %s): %s"
+            print >> sys.stderr, fmt % (input_graph, str_ans_corr, str_ans_out, e)
+
+    return (0 if outcome==CORRECT else -1)
 
 def main(argv=sys.argv[1:]):
     usage = """usage: %prog [options] INPUT_GRAPH OUTPUT_TO_CHECK
 Checks the validity of an MST.  Exits with code 0 on success.  Otherwise, it
-prints an error message and exits with a non-zero code."""
+prints an error message and exits with a non-zero code.  Does not log the result."""
     parser = OptionParser(usage)
     parser.add_option("-f", "--force-recompute",
                       action="store_true", default=False,
@@ -110,9 +118,6 @@ prints an error message and exits with a non-zero code."""
     parser.add_option("-t", "--tolerance",
                       type="int", default=1,
                       help="number of decimal places of the weight check which must match exactly [default: %default]")
-    parser.add_option("-x", "--dont-log",
-                      action="store_true", default=False,
-                      help="do not log the result")
 
     (options, args) = parser.parse_args(argv)
     if len(args) < 2:
@@ -122,7 +127,7 @@ prints an error message and exits with a non-zero code."""
 
     input_graph = args[0]
     output_to_test = args[1]
-    return check(input_graph, output_to_test, options.tolerance, options.force_recompute, not options.dont_log)
+    return check(input_graph, output_to_test, options.tolerance, options.force_recompute)
 
 if __name__ == "__main__":
     sys.exit(main())
