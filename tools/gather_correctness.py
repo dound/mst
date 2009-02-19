4b0237a2c32192f7c01d212bd9986c0a38cbf6fe
David Underhill <dgu@cs.stanford.edu>
2009-02-18 18:14:10 -0800

    give us an idea of where in correctness we are

--- tools/gather_correctness.py
+++ tools/gather_correctness.py
@@ -3,7 +3,7 @@
 from data import DataSet, InputSolution
 from check_output import get_and_log_mst_weight_from_checker
 from generate_input import main as generate_input
-import sys
+import sys, time
 
 if len(sys.argv) != 2:
     print 'usage: gather_correctness.py LOG_FN'
@@ -16,11 +16,13 @@ ds = DataSet.read_from_file(InputSolution, logfn)
 # compute correctness for each input
 inputs = ds.dataset.keys() # Input objects
 inputs.sort()
+on = 0
 for i in inputs:
+    on += 1
     # figure out how to generate the graph and where it will be sotred
     argstr = '-mt ' + i.make_args_for_generate_input()
     input_graph = generate_input(argstr.split(), get_output_name_only=True)
-    print 'gathering correctness data for ' + argstr
+    print time.ctime(time.time()) + ' input # ' + str(on) + ' => gathering correctness data for ' + argstr
 
     # generate the graph
     generate_input(argstr.split())
