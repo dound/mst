6502d3626ff440df160c10a0db1c889e36c71a37
David Underhill <dgu@cs.stanford.edu>
2009-02-19 11:27:49 -0800

    back to normal; use BEST_ALG; updated tracked revs

--- tools/data_collector.py
+++ tools/data_collector.py
@@ -217,7 +217,7 @@ def collect_data(revs, get_results_for_rev, inputs, collect_missing_data, num_ru
             print 'working on input #%u' % on
 
             # hack to prevent us from accidentally doing a crazy prims problem
-            if rev == 'da9b46bdc0' or rev == 'fc05f63801':
+            if rev == 'da9b46bdc0' or rev == 'fc05f63801' or rev == 'b0556b5bdc':
                 if i.num_verts > 8000:
                     print 'refusing to do prims with |V| > 8000 (skip!): ' + str(i)
                     continue
