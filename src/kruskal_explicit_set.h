920028457c48667540adc8a187a4f0dd8a75e4f6
Derrick Paul Isaacson <derricki@myth10.Stanford.EDU>
2009-02-20 02:59:07 -0800

    use realloc instead of malloc, and remove initial allocations for all nodes

--- src/kruskal_explicit_set.h
+++ src/kruskal_explicit_set.h
@@ -10,7 +10,7 @@
 #include <string.h> /* memcpy */
 
 // in EXPLICIT_SET version, initial size of each set to malloc
-#define INIT_CC_CAP 2
+#define INIT_CC_CAP 4
 
 // set of vertices
 typedef struct
@@ -44,11 +44,8 @@ void makeUnionFind(int n)
     CCs = (UFSet *)malloc((n+1)*sizeof(UFSet));
     for (i = 0; i < n+1; i++)
     {
-        CCs[i].vertices = (int *)malloc(INIT_CC_CAP*sizeof(int));
-        assert(CCs[i].vertices);
-        CCs[i].capacity = INIT_CC_CAP;
+        CCs[i].capacity = 1;
         CCs[i].size = 1;
-        CCs[i].vertices[0] = i;
     }
 }
 
@@ -65,25 +62,41 @@ void unionCCs(int a, int b)
 void set_parent_explicit(int a, int b)
 {
     // grow array if necessary
-      if (CCs[b].capacity < CCs[a].size + CCs[b].size)
+    if (CCs[b].capacity < CCs[a].size + CCs[b].size)
     {
-        int *oldParent = CCs[b].vertices;
-        CCs[b].capacity *= 2;
-        CCs[b].vertices = (int *)malloc(CCs[b].capacity * sizeof(int));
-        assert(CCs[b].vertices);
-        memcpy(CCs[b].vertices, oldParent, CCs[b].size*sizeof(int));
-        free(oldParent);
+        if (CCs[b].capacity == 1)
+        {
+            CCs[b].capacity = INIT_CC_CAP;
+            CCs[b].vertices = (int *)malloc(INIT_CC_CAP * sizeof(int));
+            CCs[b].vertices[0] = b;
+        }
+        else
+        {
+            CCs[b].capacity *= 2;
+            CCs[b].vertices = (int *)realloc(CCs[b].vertices,
+                                             CCs[b].capacity * sizeof(int));
+        }
     }
-
-    // set new parent values
-    int i;
-    for (i = 0; i < CCs[a].size; i++)
-        exp_parents[CCs[a].vertices[i]] = b;
-
-    // copy over values in set
-    memcpy(CCs[b].vertices+CCs[b].size, CCs[a].vertices, CCs[a].size*sizeof(int));
-    free(CCs[a].vertices);
-    CCs[b].size += CCs[a].size;
+    
+    if (CCs[a].size == 1)
+    {
+        exp_parents[a] = b;
+        CCs[b].vertices[CCs[b].size] = a;
+        CCs[b].size++;
+    }
+    else
+    {
+        // set new parent values
+        int i;
+        for (i = 0; i < CCs[a].size; i++)
+            exp_parents[CCs[a].vertices[i]] = b;
+        
+        // copy over values in set
+        memcpy(CCs[b].vertices+CCs[b].size, CCs[a].vertices,
+               CCs[a].size*sizeof(int));
+        free(CCs[a].vertices);
+        CCs[b].size += CCs[a].size;
+    }
 }
 
 int find(int u)
